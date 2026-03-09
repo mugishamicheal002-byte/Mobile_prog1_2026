e0be832d7fd15c1406100a247816df0a8096eb42
mugishamicheal002 <mugishamicheal002@gmail.com>
2026-03-09 19:41:30 +0300

    Commit to secondary branch

--- app/src/main/java/com/example/welcome/MainActivity.kt
+++ app/src/main/java/com/example/welcome/MainActivity.kt
@@ -4,9 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.welcome.ui.theme.NdejjeWelcomeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,7 +39,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NdejjeWelcomeAppTheme {
-                StudentInfo()
+                StudentIdCard()
 
 
                 }
@@ -25,16 +49,85 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun StudentInfo(){
-    Text(text="Wangadya Rusten")
-    Text(text="Bachelor of Computer Science")
-    Text(text="REG NO.:")
-    Text(text="24/2/306/D/049")
+    val profileImage = painterResource(R.drawable.mine1)
+    val logoImage = painterResource(R.drawable.logo)
+
+    Column (
+        modifier = Modifier.padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Box {
+            Image(
+                painter = profileImage,
+                contentDescription = "mine1",
+                modifier = Modifier
+                    .clip(RoundedCornerShape(10))
+            )
+            Image(
+                painter = logoImage,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(80.dp)
+                    .align(Alignment.BottomEnd)
+                    .padding(4.dp)
+
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = stringResource(R.string.wangadya_rusten),
+            style = MaterialTheme.typography.headlineSmall,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary
+        )
+
+            Text(
+                text = stringResource(R.string.programme),
+                style = MaterialTheme.typography.labelMedium,
+                color = MaterialTheme.colorScheme.primary
+            )
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 12.dp, horizontal = 24.dp),
+            thickness=1.dp,
+            color = MaterialTheme.colorScheme.outlineVariant
+        )
+        Row {
+            Text(
+                text = "REG NO.:",
+                style = MaterialTheme.typography.labelLarge,
+                fontWeight = FontWeight.ExtraBold
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(
+                text = stringResource(R.string.reg_no),
+                style = MaterialTheme.typography.bodyLarge,
+                fontFamily = FontFamily.Monospace
+            )
+        }
+    }
 }
-@Preview(showBackground = true)
+@Composable
+fun StudentIdCard() {
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+            shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.elevatedCardColors(
+            MaterialTheme.colorScheme.surfaceVariant.copy(0.3f)
+        )
+
+    ) {
+StudentInfo()
+    }
+}
+@Preview(showBackground = true,
+    showSystemUi = true)
 @Composable
 fun WelcomePreview(){
     NdejjeWelcomeAppTheme {
-        StudentInfo()
+        StudentIdCard()
     }
 
 }
